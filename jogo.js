4282be307b26bb8cc978f01355cec42677337de2
juliobersantos25 <104808707+juliobersantos25@users.noreply.github.com>
2022-06-24 21:47:10 -0300

    Trabalho CSS , JAVA .
    24/06/2022

---
+++ jogo.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+[{"id": 1, "camponês": "vila", "casa": "fiona", "tribo muito feroz": "vivem na cidade ",  "shek":  ["FULANO",  "CICLADO",  "BELTRANO"], "imagem": "https://observatoriodatv.uol.com.br/noticias/sbt-exibe-o-filme-shrek-no-cine-espetacular"}]
+[{"id": 1, "guerreiro": "cidade", "adega": "fiona", "tribo muito feroz": "vivem no bar",  "shek":  ["FULANO",  "CICLADO",  "BELTRANO"], "imagem": "https://observatoriodatv.uol.com.br/noticias/sbt-exibe-o-filme-shrek-no-cine-espetacular"}]
+[{"id": 1, "feiticeiro": "montanhas", "barraco": "fiona", "tribo muito feroz": "vivem nas montanhas",  "shek":  ["FULANO",  "CICLADO",  "BELTRANO"], "imagem": "https://observatoriodatv.uol.com.br/noticias/sbt-exibe-o-filme-shrek-no-cine-espetacular"}]
+[{"id": 1, "ogro": "pantano", "barraco": "fiona", "tribo muito feroz": "vivem no pantano",  "shek":  ["FULANO",  "CICLADO",  "BELTRANO"], "imagem": "https://observatoriodatv.uol.com.br/noticias/sbt-exibe-o-filme-shrek-no-cine-espetacular"}]
